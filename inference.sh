902f4cbe96855edb64b0208a3aebf02321d4a136
Adriana Bukala <bukala.adriana@gmail.com>
2025-02-13 15:38:38 +0100

    fix(inference): unify output's column names

--- inference.sh
+++ inference.sh
@@ -1,6 +1,4 @@
 inputpath="$1"
 outputpath="$2"
 
-python src/AMPlify.py -s "$inputpath" -on "$outputpath" &&
-
-python convertoutputs.py "$outputpath" "$outputpath"
+python src/AMPlify.py -s "$inputpath" -on "$outputpath"
